3aef20d5fbae9e5cff37e2f9a6eae0ff42a2e7dd
Claudio Xavier <claudioxavier@MacBook-Air-de-Claudio.local>
2025-05-30 13:30:25 -0300

    ajustes na autorização e retorno de json em form request

--- app/Http/Requests/AutorRequest.php
+++ app/Http/Requests/AutorRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class AutorRequest extends FormRequest
 {
@@ -11,7 +13,7 @@ class AutorRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,5 +26,14 @@ class AutorRequest extends FormRequest
         return [
             'nome' => ['required', 'string', 'max:255']
         ];
+    }
+
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'error' => $validator->errors()
+        ]));
     }
 }
--- app/Http/Requests/LivroRequest.php
+++ app/Http/Requests/LivroRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class LivroRequest extends FormRequest
 {
@@ -11,7 +13,7 @@ class LivroRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -32,5 +34,13 @@ class LivroRequest extends FormRequest
             'assunto_ids' => ['required', 'array', 'min:1'],
             'assunto_ids.*' => ['exists:assuntos,id'],
         ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'error' => $validator->errors()
+        ]));
     }
 }
